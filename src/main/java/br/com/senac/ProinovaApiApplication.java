3503bed28ea149b01d57310522c161daf82e2277
ALEX BARANOSKI SPECK <alexbaranoski08@gmail.com>
2025-11-14 20:48:19 -0300

    primeiro commit

---
+++ src/main/java/br/com/senac/ProinovaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProinovaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProinovaApiApplication.class, args);
+	}
+
+}
