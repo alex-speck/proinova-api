43531014e1678be334aad2bc3823e4c85028cdb1
alex-speck <alexbaranoski08@gmail.com>
2025-12-09 00:00:32 -0300

    userresponse correction

--- src/main/java/br/com/senac/dto/UserResponseDTO.java
+++ src/main/java/br/com/senac/dto/UserResponseDTO.java
@@ -5,6 +5,6 @@ import br.com.senac.entity.enums.UserRole;
 
 public record UserResponseDTO(Long id, String email, String username, String name, UserRole role, String userProfilePictureUrl) {
     public UserResponseDTO(User user){
-        this(user.getId(), user.getUsername(), user.getEmail(), user.getName(), user.getRole(), user.getProfilePictureURL());
+        this(user.getId(), user.getEmail(), user.getUsername(), user.getName(), user.getRole(), user.getProfilePictureURL());
     }
 }
