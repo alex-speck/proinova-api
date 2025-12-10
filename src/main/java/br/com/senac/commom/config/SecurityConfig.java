1a04b6a79cf4b28cdd18793456c551e3b5482d5e
Alex Baranoski Speck <alexbaranoski08@gmail.com>
2025-12-10 13:22:09 -0300

    Atualizar o SecurityConfig.java

--- src/main/java/br/com/senac/commom/config/SecurityConfig.java
+++ src/main/java/br/com/senac/commom/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS,  "/**").permitAll()
+.requestMatchers(HttpMethod.GET, "/uploads/**").permitAll();
                         // Rotas de Login
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
